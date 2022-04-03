e1efce2e45208ecfc42f5057f82bcb5360ba3705
Alviannn <alvianndq@gmail.com>
2022-04-03 19:57:51 +0700

    Add initial menu

--- src/com/github/argajuvi/Main.java
+++ src/com/github/argajuvi/Main.java
@@ -1,9 +1,55 @@
 package com.github.argajuvi;
 
+import com.github.argajuvi.utils.Utils;
+
 public class Main {
 
     public Main() {
+        while (true) {
+            Utils.clearScreen();
 
+            System.out.println(
+                    " █████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗     ███████╗██╗  ██╗ ██████╗ ██████╗ \n" +
+                    "██╔══██╗╚██╗ ██╔╝██╔═══██╗██╔══██╗██╔══██╗    ██╔════╝██║  ██║██╔═══██╗██╔══██╗\n" +
+                    "███████║ ╚████╔╝ ██║   ██║██████╔╝███████║    ███████╗███████║██║   ██║██████╔╝\n" +
+                    "██╔══██║  ╚██╔╝  ██║   ██║██╔═══╝ ██╔══██║    ╚════██║██╔══██║██║   ██║██╔═══╝ \n" +
+                    "██║  ██║   ██║   ╚██████╔╝██║     ██║  ██║    ███████║██║  ██║╚██████╔╝██║     \n" +
+                    "╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     \n");
+
+            System.out.println(
+                    "1. Login\n" +
+                    "2. Register\n" +
+                    "0. Exit\n");
+
+            int choice;
+            while (true) {
+                System.out.print(">> ");
+                choice = Utils.scanInt(-1);
+
+                if (choice == -1) {
+                    System.out.println("Input must be integer");
+                    continue;
+                }
+
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    // TODO: run login menu
+                    break;
+                case 2:
+                    // TODO: run register menu
+                    break;
+                case 0:
+                    System.exit(0);
+                    return;
+                default:
+                    System.out.println("Option is not available!");
+                    Utils.waitForEnter();
+                    break;
+            }
+        }
     }
 
     public static void main(String[] args) {
