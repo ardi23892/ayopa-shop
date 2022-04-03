b3001ddb446f4edf688b485f9cbcf6e4b68f7917
Alviannn <alvianndq@gmail.com>
2022-04-03 20:36:24 +0700

    * Prepare all (currently remembered) menu * Add user menu

--- src/com/github/argajuvi/Main.java
+++ src/com/github/argajuvi/Main.java
@@ -52,6 +52,64 @@ public class Main {
         }
     }
 
+    private void showLogin() {
+        throw new UnsupportedOperationException("Login menu has yet been build");
+    }
+
+    private void showRegister() {
+        throw new UnsupportedOperationException("Register menu has yet been build");
+    }
+
+    private void showUserMenu() {
+        while (true) {
+            Utils.clearScreen();
+
+            System.out.println(
+                    "Welcome, <NAME> to AyopaShop\n" +
+                    "-------------------------\n" +
+                    "1. Check purchase history\n" +
+                    "2. Buy products\n" +
+                    "3. Logout\n" +
+                    "0. Exit\n");
+
+            int choice;
+            while (true) {
+                System.out.print(">> ");
+                choice = Utils.scanInt(-1);
+
+                if (choice == -1) {
+                    System.out.println("Input must be integer");
+                    continue;
+                }
+
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    // TODO: user checks their purchase history (using struct)
+                    break;
+                case 2:
+                    // TODO: user starts to buy products
+                    break;
+                case 3:
+                    // TODO: logout from current user
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Option is not available!");
+                    Utils.waitForEnter();
+                    break;
+            }
+        }
+    }
+
+    private void showAdminMenu() {
+        throw new UnsupportedOperationException("Admin menu has yet been build");
+    }
+
     public static void main(String[] args) {
         new Main();
     }
