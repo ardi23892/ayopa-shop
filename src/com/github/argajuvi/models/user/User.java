9aab8f878c1da147ea2d2c29bbf056400cac38a1
Alviannn <alvianndq@gmail.com>
2022-04-03 22:54:59 +0700

    * Fix inconsistency tab with spacing in User class * Fix missing package in User class

--- src/com/github/argajuvi/models/user/User.java
+++ src/com/github/argajuvi/models/user/User.java
@@ -1,25 +1,29 @@
+package com.github.argajuvi.models.user;
 
 public class User {
-	
-	private String username;
-	private String password;
-	
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
