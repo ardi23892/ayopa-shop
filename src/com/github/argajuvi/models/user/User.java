15bab6579fdc764b444ac4b798f5c7b161035118
Alviannn <alvianndq@gmail.com>
2022-04-04 07:35:32 +0700

    Add class relationship

--- src/com/github/argajuvi/models/user/User.java
+++ src/com/github/argajuvi/models/user/User.java
@@ -1,29 +1,39 @@
 package com.github.argajuvi.models.user;
 
+import com.github.argajuvi.models.order.Order;
+import com.github.argajuvi.models.receipt.Receipt;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
-    private String username;
-    private String password;
+    private final String username, password;
+    private final List<Receipt> receiptList;
+    private final List<Order> currentCart;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+
+        this.receiptList = new ArrayList<>();
+        this.currentCart = new ArrayList<>();
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public List<Receipt> getReceiptList() {
+        return receiptList;
+    }
+
+    public List<Order> getCurrentCart() {
+        return currentCart;
     }
 
 }
