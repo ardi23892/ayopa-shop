b4e91f78900c2f4c5e4bae7208f00db3aca61036
Alviannn <alvianndq@gmail.com>
2022-06-13 19:14:40 +0700

    Remove unnecessary parameters

--- src/com/github/argajuvi/menus/InitialMenu.java
+++ src/com/github/argajuvi/menus/InitialMenu.java
@@ -3,6 +3,7 @@ package com.github.argajuvi.menus;
 import com.github.argajuvi.Main;
 import com.github.argajuvi.database.Database;
 import com.github.argajuvi.models.user.User;
+import com.github.argajuvi.utils.Closer;
 import com.github.argajuvi.utils.Utils;
 
 import java.sql.ResultSet;
@@ -13,9 +14,9 @@ import java.util.Scanner;
 
 public class InitialMenu implements Menu {
 
-	Database db = Database.getInstance();
-	List<User> userList = Main.USER_LIST;
-	
+    Database db = Database.getInstance();
+    List<User> userList = Main.USER_LIST;
+
     @Override
     public void showMenu() throws ParseException {
 
@@ -38,10 +39,10 @@ public class InitialMenu implements Menu {
             int choice = Utils.scanAbsoluteInt(">> ");
             switch (choice) {
                 case 1:
-                    this.loginUser(userList);
+                    this.loginUser();
                     break;
                 case 2:
-                    userList.add(this.registerUser(userList));
+                    userList.add(this.registerUser());
                     break;
                 case 0:
                     System.exit(0);
@@ -55,7 +56,7 @@ public class InitialMenu implements Menu {
     }
 
     //	FUNCTION UNTUK LOGIN USER
-    public void loginUser(List<User> users) throws ParseException {
+    public void loginUser() throws ParseException {
         Scanner scanner = Utils.SCANNER;
 
         String username;
@@ -66,50 +67,48 @@ public class InitialMenu implements Menu {
         username = scanner.nextLine();
         System.out.print("Input Password: ");
         password = scanner.nextLine();
-        
+
         //VALIDASI DATA LOGIN
-        String query;
-        
         boolean usernameCheck = false;
         String currPassword = "";
-        
+
         int userIdx = 0;
         try {
-        	ResultSet rs = db.getResults("SELECT * FROM users WHERE username = ?", username);
-			while(rs.next()) {
-				//USERNAME DITEMUKAN
-				Main.userId = rs.getInt("id");
-				usernameCheck = true;
-				for(int i = 0; i < userList.size(); i++) {
-					if(username.equals(userList.get(i).getUsername())) userIdx = i;
-				}
-				currPassword = rs.getString("password");
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-        
-        if(!usernameCheck){
-        	System.out.println("Username doesn't exists! Make sure the account is registered!");
-        	Utils.waitForEnter();
+            ResultSet rs = db.getResults("SELECT * FROM users WHERE username = ?", username);
+            while (rs.next()) {
+                //USERNAME DITEMUKAN
+                Main.userId = rs.getInt("id");
+                usernameCheck = true;
+                for (int i = 0; i < userList.size(); i++) {
+                    if (username.equals(userList.get(i).getUsername())) userIdx = i;
+                }
+                currPassword = rs.getString("password");
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
         }
-        
-        
-        if(currPassword.equals(password)) {
-        	if(username.equals("admin")) {
-				menu = new AdminMenu();
-			}else {
-				menu = new UserMenu();
-			}
-        	
-        	Main.CURRENT_USER = userList.get(userIdx);
-        	menu.showMenu();
+
+        if (!usernameCheck) {
+            System.out.println("Username doesn't exists! Make sure the account is registered!");
+            Utils.waitForEnter();
         }
-   
+
+
+        if (currPassword.equals(password)) {
+            if (username.equals("admin")) {
+                menu = new AdminMenu();
+            } else {
+                menu = new UserMenu();
+            }
+
+            Main.CURRENT_USER = userList.get(userIdx);
+            menu.showMenu();
+        }
+
     }
 
     //	FUNCTION REGISTER DATA USER BARU
-    public User registerUser(List<User> users) {
+    public User registerUser() {
         Scanner scanner = Utils.SCANNER;
         boolean registering = true;
 
@@ -127,36 +126,34 @@ public class InitialMenu implements Menu {
 
             User newUser = new User(username, password);
             registering = insertUser(newUser);
-            
+
         } while (registering);
 
         System.out.println("User is successfully registered!");
         Utils.waitForEnter();
-        
+
         return new User(username, password);
     }
-    
+
     //Insert Query with Unique user
     public boolean insertUser(User newUser) {
-    	String query;
-        
-        boolean isUnique = true;
-        try {
-        	ResultSet rs = db.getResults("SELECT * FROM users WHERE username = ?", newUser.getUsername());
-			while(rs.next()) {
-				isUnique = false;
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-        
-        if(isUnique) {
-        	db.execute("INSERT INTO users VALUES(NULL, ?, ?)", newUser.getUsername(), newUser.getPassword());    			
-        }else {
-        	System.out.println("User has already been registered before, maybe try login with it?");
+        boolean isNotRegistered = true;
+
+        try (Closer closer = new Closer()) {
+            ResultSet rs = closer.add(db.getResults("SELECT * FROM users WHERE username = ?", newUser.getUsername()));
+            isNotRegistered = rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (isNotRegistered) {
+            db.execute("INSERT INTO users VALUES (NULL, ?, ?)", newUser.getUsername(), newUser.getPassword());
+        } else {
+            System.out.println("User has already been registered before, maybe try login with it?");
             Utils.waitForEnter();
             return true;
         }
+
         return false;
     }
 
